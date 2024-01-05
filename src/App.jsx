01565b020bc4a81f0ab8e7574a9088013e899b78
Said <shahrokh07.96@gmail.com>
2024-01-05 17:05:40 +0530

    avoide redundant state

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 // import { useImmer } from 'use-immer';
 import { useEffect, useState } from "react";
-import FeedbackForm from "./FeedbackForm.jsx";
+import Form from "./Form.jsx";
 
 let statuses = [
   'empty',
@@ -16,7 +16,7 @@ export default function App() {
 
   return (
   <>
-    <FeedbackForm />
+    <Form />
   </>
   );
 }
